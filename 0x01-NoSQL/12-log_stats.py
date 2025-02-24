3bd3aa167b2b3450994cafe7765e4842fe99720f
Parsitau-michael <parsitaumichael@gmail.com>
2025-02-24 05:32:53 -0800

    Enclosed my script in a function

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,19 +5,26 @@ a Python script that provides some stats about Nginx logs stored in MongoDB
 
 
 from pymongo import MongoClient
-client = MongoClient()
 
 
-"""
-a Python script that provides some stats about Nginx logs stored in MongoDB
-"""
-collection = client.logs.nginx
-print("{} logs".format(collection.count_documents({})))
-print("Methods:")
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in methods:
-    count = collection.count_documents({"method": method})
-    print("\tmethod {}: {}".format(method, count))
+def log_stats():
+    """
+    Fetch and display statistics from the 'nginx' collection
+    in the 'logs' database.
+    """
+    client = MongoClient()
+    collection = client.logs.nginx
 
-status_count = collection.count_documents({"method": "GET", "path": "/status"})
-print("{} status check".format(status_count))
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+    stats = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status_count))
+
+
+if __name__ == "__main__":
+    log_stats()
