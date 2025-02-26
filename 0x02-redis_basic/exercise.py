8d7e7e9da50b563f4405e9088a245d1337dd1498
Parsitau-michael <parsitaumichael@gmail.com>
2025-02-26 20:18:13 +0000

    Storing lists in redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,6 +21,19 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Defining a decorator to store the history of inputs
+    and outputs for a particular function.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.rpush(method.__qualname__ + ":inputs", str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(method.__qualname__ + ":outputs", str(output))
+        return output
+    return wrapper
+
+
 class Cache:
     """Cache class using Redis."""
     def __init__(self):
@@ -28,6 +41,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in Redis with a randomly generated key."""
