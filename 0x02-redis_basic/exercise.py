e7e778f24f3ddea85cf109f9197ddf9c087dca81
Parsitau-michael <parsitaumichael@gmail.com>
2025-02-26 08:52:33 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ This module creates and initializes a cache class
 
 import uuid
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -20,3 +20,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], any]]
+            = None) -> Optional[any]:
+        """Retrieve data from Redis and optionally apply a conversion function.
+        """
+        response = self._redis.get(key)
+        if response is None:
+            return None
+        return fn(response) if fn else response
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve value as a string."""
+        return self.get(key, lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve value as an integer."""
+        return self.get(key, int)
